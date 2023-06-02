5f7d8a2957c77ae8005b41e45d1603b775d5392b
dengqizhang <2435563017@qq.com>
2023-06-02 11:53:02 +0800

    dev1.4"完成了购物车的更新接口"

--- MoheBasketball-plus/user_home/user_vipuser/src/main/java/org/example/mapper/VipUserMapper.java
+++
@@ -1,8 +0,0 @@
-package org.example.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-import org.example.model.VipUserDto;
-@Mapper
-public interface VipUserMapper extends BaseMapper<VipUserDto> {
-}
--- MoheBasketball-plus/user_home/user_vipuser/src/main/java/org/example/service/VipUserService.java
+++
@@ -1,9 +0,0 @@
-package org.example.service;
-
-import org.example.model.VipUserDto;
-
-import java.util.List;
-
-public interface VipUserService {
-    List<VipUserDto> findAllList();
-}
